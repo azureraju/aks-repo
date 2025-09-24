bbb394faf3ff8483f3f278c62df4821f0bc18089
azureraju <azureraju555@gmail.com>
2025-09-24 14:34:05 +0530

    Initial commit - Terraform AKS setup

---
+++ main.tf
@@ -0,0 +1,58 @@
+
+# Resource Group
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "aks-rg"
+  location = "East US"
+}
+
+
+# Virtual Network & Subnet (Optional but recommended)
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "aks-vnet"
+  address_space       = ["10.0.0.0/8"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.240.0.0/16"]
+}
+
+
+# AKS Cluster
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksdemo"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"   # Or "kubenet"
+    load_balancer_sku = "standard"
+    outbound_type     = "loadBalancer"
+  }
+}
+
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
